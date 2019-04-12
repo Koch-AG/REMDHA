6d88148a2da5fad507f9d3e351458e2a95c8f98f
Nathanael Barben <nbarben@kochag.ch>
2019-04-12 16:04:18 +0200

    initial commit

---
+++ REMDHA/main.c
@@ -0,0 +1,18 @@
+/*
+ * REMDHA.c
+ *
+ * Created: 12.04.2019 15:55:21
+ * Author : nbarben
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
