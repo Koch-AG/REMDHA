4bf15922f9d30537e521509d8af2bae88919abe0
Nathanael Barben <nbarben@kochag.ch>
2019-04-16 10:14:04 +0200

    add Init and read function for PAJ7620

--- REMDHA/sensor.h
+++ REMDHA/sensor.h
@@ -13,8 +13,21 @@
 #define MGC3130_ID_READ		(MGC3130_ID << 1) + 1
 #define MGC3130_ID_WRITE	(MGC3130_ID << 1) + 0
 
+#define PAJ7620_ID			0x73						//I2C ID PAJ7620
+#define PAJ7620_ID_READ		(PAJ7620_ID << 1) + 1
+#define PAJ7620_ID_WRITE	(PAJ7620_ID << 1) + 0
+
+//MGC3130//////////////////////////////////////////////////////////////////////////////////////////
 bool MGC3130Init(void);									//initialize MGC3130
-bool MGC3130Read(void);									//read gesture from MGC3130
+bool MGC3130ReadGesture(void);							//read gesture from MGC3130
+
+
+	
+	
+//PAJ7620//////////////////////////////////////////////////////////////////////////////////////////
+void PAJ7620Init(void);									//initialize PAJ7620
+void PAJ7620ReadGesture (void);							//read gesture
+
 
 
 
